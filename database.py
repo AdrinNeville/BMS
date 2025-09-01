bc51955cd30674a2c4fcc2f14817330c99a9114d
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-09-01 15:08:38 +0530

    without authorisation

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ MONGO_URL = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.library_db
 
-# Helper: convert ObjectId → str (for JSON responses)
+# helper to convert ObjectId → str
 def obj_to_str(obj):
     if isinstance(obj, ObjectId):
         return str(obj)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from routers import users, books, borrow, auth
+from routers import users, books, borrow
 
-app = FastAPI(title="Library Management with Auth")
+app = FastAPI(title="Library Management with MongoDB")
 
-app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(books.router)
 app.include_router(borrow.router)
